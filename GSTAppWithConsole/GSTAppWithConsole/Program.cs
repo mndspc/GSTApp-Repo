869d5779e8592dbdc7bf1fae5fe22b0cb3112a67
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-08-19 10:16:25 +0530

    Added a New Project
    Demo contains another project

---
+++ GSTAppWithConsole/GSTAppWithConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GST;
+
+namespace GSTAppWithConsole
+{
+    class Program
+    {
+        //  This program demo. how consume classes from .dll/private assembly
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Product Amount:");
+            double proAmt = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter GST Per:");
+            double gstPer = Convert.ToDouble(Console.ReadLine());
+            GSTCalc gSTCalc = new GSTCalc(proAmt, gstPer);
+            Console.WriteLine("Product Amount with GST is:{0}", gSTCalc.AmtIncludingGst());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GSTAppWithConsole/GSTAppWithWinform/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GST;
+namespace GSTAppWithWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double proAmt =Convert.ToDouble( textBox1.Text);
+            double gstPer = Convert.ToDouble(textBox2.Text);
+            GSTCalc gSTCalc = new GSTCalc(proAmt, gstPer);
+            double result = gSTCalc.AmtIncludingGst();
+            MessageBox.Show(string.Format("Product Amount is:{0}",result));
+        }
+    }
+}
